2ad99bac84472d6ce825947195d62e9a0b4f3734
“xinqiyi1024” <3633414863@qq.com>
2021-10-26 22:39:45 +0800

    new linklist update .sln

---
+++ 2020002309_方泽楠_linklist/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020002309_方泽楠_linklist
+{
+    class Node<T>
+    {
+        public T Date;
+        public Node<T> Next;
+
+        public Node(T item)
+        {
+            this.Date = item;
+            this.Next = null;
+        }
+
+        public Node()
+        {
+            this.Date = default(T);
+            this.Next = null;
+        }
+    }
+}
