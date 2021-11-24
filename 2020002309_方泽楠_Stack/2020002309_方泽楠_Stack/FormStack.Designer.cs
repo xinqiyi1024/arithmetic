2d2a43f68a84939f04d9244f0185ed3608be9946
“xinqiyi1024” <3633414863@qq.com>
2021-11-24 12:58:11 +0800

    update

---
+++ 2020002309_方泽楠_Stack/2020002309_方泽楠_Stack/FormStack.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace _2020002309_方泽楠_Stack
+{
+    partial class FormStack
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb_num = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lb_carNum = new System.Windows.Forms.Label();
+            this.lb_pay = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lb_stopTime = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_num = new System.Windows.Forms.Label();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当前场内空车位：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(52, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(258, 208);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(137, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "停车场出入口";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_num);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(378, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "停车入场";
+            // 
+            // tb_num
+            // 
+            this.tb_num.Location = new System.Drawing.Point(156, 36);
+            this.tb_num.Name = "tb_num";
+            this.tb_num.Size = new System.Drawing.Size(100, 21);
+            this.tb_num.TabIndex = 9;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(104, 71);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 8;
+            this.btn1.Text = "确定停车";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "车牌号码";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lb_carNum);
+            this.groupBox2.Controls.Add(this.lb_pay);
+            this.groupBox2.Controls.Add(this.btn2);
+            this.groupBox2.Controls.Add(this.lb_stopTime);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(378, 207);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 134);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "缴费离场";
+            // 
+            // lb_carNum
+            // 
+            this.lb_carNum.AutoSize = true;
+            this.lb_carNum.Location = new System.Drawing.Point(20, 60);
+            this.lb_carNum.Name = "lb_carNum";
+            this.lb_carNum.Size = new System.Drawing.Size(29, 12);
+            this.lb_carNum.TabIndex = 13;
+            this.lb_carNum.Text = "0.00";
+            // 
+            // lb_pay
+            // 
+            this.lb_pay.AutoSize = true;
+            this.lb_pay.Location = new System.Drawing.Point(194, 60);
+            this.lb_pay.Name = "lb_pay";
+            this.lb_pay.Size = new System.Drawing.Size(29, 12);
+            this.lb_pay.TabIndex = 12;
+            this.lb_pay.Text = "0.00";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(104, 93);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "确定离场";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lb_stopTime
+            // 
+            this.lb_stopTime.AutoSize = true;
+            this.lb_stopTime.Location = new System.Drawing.Point(102, 60);
+            this.lb_stopTime.Name = "lb_stopTime";
+            this.lb_stopTime.Size = new System.Drawing.Size(29, 12);
+            this.lb_stopTime.TabIndex = 11;
+            this.lb_stopTime.Text = "0.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "车牌号";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(194, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "应缴费用";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(102, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "停车时长";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "当前时间";
+            // 
+            // lb_num
+            // 
+            this.lb_num.AutoSize = true;
+            this.lb_num.Location = new System.Drawing.Point(229, 30);
+            this.lb_num.Name = "lb_num";
+            this.lb_num.Size = new System.Drawing.Size(0, 12);
+            this.lb_num.TabIndex = 7;
+            // 
+            // lb_time
+            // 
+            this.lb_time.AutoSize = true;
+            this.lb_time.Location = new System.Drawing.Point(498, 30);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(0, 12);
+            this.lb_time.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormChoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 408);
+            this.Controls.Add(this.lb_time);
+            this.Controls.Add(this.lb_num);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormChoice";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lb_pay;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lb_stopTime;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_num;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tb_num;
+        private System.Windows.Forms.Label lb_carNum;
+    }
+}
+
--- 2020002309_方泽楠_Stack/2020002309_方泽楠_Stack/Program.cs
+++ 2020002309_方泽楠_Stack/2020002309_方泽楠_Stack/Program.cs
@@ -15,7 +15,8 @@ namespace _2020002309_方泽楠_Stack
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            new FormChoice().Show();
+            Application.Run();
         }
     }
 }
